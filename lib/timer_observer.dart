fc4c109d2ef28721f2f82a54fa4a6af88ead3d6f
Aaron Kelton <6170077+aaronkelton@users.noreply.github.com>
2023-06-23 13:06:40 -0500

    created the observer file for change output

---
+++ lib/timer_observer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TimerObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
+  }
+
+}
